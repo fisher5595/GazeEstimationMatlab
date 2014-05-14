036b051ba1729d68aa9a759adbadce10f218e979
fisher5595 <fisher5595@gmail.com>
2014-05-13 23:10:21 -0500

    [MFMC]:[LearnConstraintXeXc]:Correct one error of the direction of gradient in EdgeMap

--- 50times100/ObservationValue_Contour_Plus_Outter_Up_Parabola.m
+++ 50times100/ObservationValue_Contour_Plus_Outter_Up_Parabola.m
@@ -3,7 +3,7 @@ function [ ObservationValue ] = ObservationValue_Contour_Plus_Outter_Up_Parabola
 %   Detailed explanation goes here
 
 %Value=0;
-ObservationValue=ObservationValue_UpParabola( EdgeMag, EdgeTheta, Xe, Theta, A, B)+ObservationValue_LowParabola( EdgeMag, EdgeTheta, Xe, Theta, C, B)+ObservationValue_OutterUpParabola( EdgeMag, EdgeTheta, Xe, Theta, A2, B2);
+ObservationValue=ObservationValue_UpParabola( EdgeMag, EdgeTheta, Xe, Theta, A, B)*ObservationValue_LowParabola( EdgeMag, EdgeTheta, Xe, Theta, C, B)*ObservationValue_OutterUpParabola( EdgeMag, EdgeTheta, Xe, Theta, A2, B2);
 
 end
 
