bd14de241821bd3ec156c816846d2b95df4bc1b6
Pei Yu @DesktopUbuntu <fisher5595@gmail.com>
2014-07-30 21:24:01 -0500

    [MFMC]:[LearnConstrainXeXc]: Add base code for new solution 1

---
+++ NewSolution1.m
@@ -0,0 +1,54 @@
+%Refer to Professor Wu's new method pdf, new solution 1.
+
+%Load features
+clear;
+k_knn=20;
+featureName='enlarged_RegisteredFeature_left_';
+
+%Load training feature matrix.
+for RoundNumber=1
+    for i = 1:36
+        feature=load([featureName,int2str(i-1),'__',int2str(RoundNumber),'.mat']);
+        featurevector=feature.x;
+        FeatureMatrix(:,i+(RoundNumber-1)*36)=featurevector; 
+    end
+end
+
+%Generate all training position information, stored in a PositionMatrix.
+for RoundNumber=1
+    for y=1:6
+        for x=1:6
+            PositionMatrix(1,(y-1)*6+x+(RoundNumber-1)*36)=floor(480/7*y);
+            PositionMatrix(2,(y-1)*6+x+(RoundNumber-1)*36)=floor(640/7*x);
+        end
+    end
+end
+
+S=FindMetricPreservationMatrix(FeatureMatrix,PositionMatrix);
+
+for QueryNumber=1:36
+    QueryFeature=FeatureMatrix(:,QueryNumber);
+    %Calculate the estimate gaze position and display it
+    FeatureVector=QueryFeature;
+    for ii = 1:36*5
+        DistanceMatrix(ii)=(FeatureVector-FeatureMatrix(:,ii))'*(FeatureVector-FeatureMatrix(:,ii));
+    end
+    [SortedDistanceMatrix,index]=sort(DistanceMatrix);
+    disp('QueryNumber');
+    disp(QueryNumber);
+    disp('index:');
+    disp(index);
+    index(find(index==QueryNumber))=[];
+
+    for k=1:k_knn
+        AMatrix(:,k)=FeatureMatrix(:,index(k));
+        TrainingWeightMatrix(:,k)=PositionMatrix(:,index(k));
+    end
+    CMatrix=(FeatureVector*ones(k_knn,1)'-AMatrix)'*S*(FeatureVector*ones(k_knn,1)'-AMatrix);
+    weight=pinv(CMatrix)*ones(k_knn,1);
+    weight=weight./sum(weight);
+    EstimatePosition=TrainingWeightMatrix*weight;
+    TotalError=TotalError+norm(double(EstimatePosition)-double(PositionMatrix(:,QueryNumber)));
+    %figure(2);
+end
+AvgError=TotalError/36;
