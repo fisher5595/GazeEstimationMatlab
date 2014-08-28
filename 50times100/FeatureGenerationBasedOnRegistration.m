9f500c99702f245c3bd6838eb07575b2699a165d
Pei Yu @DesktopUbuntu <fisher5595@gmail.com>
2014-08-28 17:47:50 -0500

    [MFMC]:[LearnConstrainXeXc]:modify generate the registered image for new data, imwarp will generate black edge. need to figure it out

--- 50times100/FeatureGenerationBasedOnRegistration.m
+++ 50times100/FeatureGenerationBasedOnRegistration.m
@@ -3,9 +3,10 @@
 % appearance feature
 
 clear;
-ImageName='enlarged_ResizedEyes_left_';
+ImageName='new_data/test_enlarged_ResizedEyes_left_';
 ImageNumber=1;
-RoundNumber=4;
+FixpointRoundNumber=1;
+RoundNumber=1;
 RightCornerImageNumber=1;
 ImageExtension='.jpg';
 QueryImageNumber=1;
@@ -13,33 +14,33 @@ Resize=[5 10];
 
 %extract eye contour parameters and get fixed points, which are the left
 %and right corner x and y coordinates of the first eye image
-Train_A=load('Training_A.mat');
+Train_A=load(['Training_Aug27_A' '__' int2str(FixpointRoundNumber) '.mat']);
 Train_A=Train_A.x;
-Train_A2=load('Training_A2.mat');
+Train_A2=load(['Training_Aug27_A2' '__' int2str(FixpointRoundNumber) '.mat']);
 Train_A2=Train_A2.x;
-Train_B=load('Training_B.mat');
+Train_B=load(['Training_Aug27_B' '__' int2str(FixpointRoundNumber) '.mat']);
 Train_B=Train_B.x;
-Train_B2=load('Training_B2.mat');
+Train_B2=load(['Training_Aug27_B2' '__' int2str(FixpointRoundNumber) '.mat']);
 Train_B2=Train_B2.x;
-Train_C=load('Training_C.mat');
+Train_C=load(['Training_Aug27_C' '__' int2str(FixpointRoundNumber) '.mat']);
 Train_C=Train_C.x;
-Train_R=load('Training_R.mat');
+Train_R=load(['Training_Aug27_R' '__' int2str(FixpointRoundNumber) '.mat']);
 Train_R=Train_R.x;
-Train_Theta=load('Training_Theta.mat');
+Train_Theta=load(['Training_Aug27_Theta' '__' int2str(FixpointRoundNumber) '.mat']);
 Train_Theta=Train_Theta.x;
-Train_Xe=load('Training_Xe.mat');
+Train_Xe=load(['Training_Aug27_Xe' '__' int2str(FixpointRoundNumber) '.mat']);
 Train_Xe=Train_Xe.x;
-Train_Xc=load('Training_Xc.mat');
+Train_Xc=load(['Training_Aug27_Xc' '__' int2str(FixpointRoundNumber) '.mat']);
 Train_Xc=Train_Xc.x;
 FixedPoints=[Train_Xe(2,1)-cos(Train_Theta(1))*Train_B2(1), Train_Xe(1,1)+sin(Train_Theta(1))*Train_B2(1); Train_Xe(2,1)+cos(Train_Theta(1))*Train_B2(1),Train_Xe(1,1)-sin(Train_Theta(1))*Train_B2(1)];
 FixedPoints=round(FixedPoints);
 YRange=[Train_Xe(1,1)-Train_A2(1);Train_Xe(1,1)+Train_C(1)+3];
 YRange=floor(YRange);
-TemplateImage=imread([ImageName,int2str(ImageNumber),ImageExtension]);
+TemplateImage=imread([ImageName,int2str(ImageNumber),'__', int2str(RoundNumber),ImageExtension]);
 CroppedImage=TemplateImage(YRange(1):YRange(2),FixedPoints(1,1):FixedPoints(2,1));
 figure(1);
 imshow(CroppedImage);
-imwrite(CroppedImage,['enlarged_registeredEyes_left_',int2str(1),'.jpg']);
+imwrite(CroppedImage,['new_data/enlarged_registeredEyes_Aug27_left_',int2str(1),'__', int2str(RoundNumber),'.jpg']);
 RegisteredResizedImage=imresize(CroppedImage, Resize);
 ImageVector=[];
 for i=1:size(RegisteredResizedImage,1)
@@ -50,30 +51,30 @@ x.x=double(ImageVector')./255;
 
 %Load labeled parameters of new round captured images. Register new round
 %images with the first eye image of the original captured eye image.
-Train_A=load(['Training_A' '__' int2str(RoundNumber) '.mat']);
+Train_A=load(['Training_Aug27_A' '__' int2str(RoundNumber) '.mat']);
 Train_A=Train_A.x;
-Train_A2=load(['Training_A2' '__' int2str(RoundNumber) '.mat']);
+Train_A2=load(['Training_Aug27_A2' '__' int2str(RoundNumber) '.mat']);
 Train_A2=Train_A2.x;
-Train_B=load(['Training_B' '__' int2str(RoundNumber) '.mat']);
+Train_B=load(['Training_Aug27_B' '__' int2str(RoundNumber) '.mat']);
 Train_B=Train_B.x;
-Train_B2=load(['Training_B2' '__' int2str(RoundNumber) '.mat']);
+Train_B2=load(['Training_Aug27_B2' '__' int2str(RoundNumber) '.mat']);
 Train_B2=Train_B2.x;
-Train_C=load(['Training_C' '__' int2str(RoundNumber) '.mat']);
+Train_C=load(['Training_Aug27_C' '__' int2str(RoundNumber) '.mat']);
 Train_C=Train_C.x;
-Train_R=load(['Training_R' '__' int2str(RoundNumber) '.mat']);
+Train_R=load(['Training_Aug27_R' '__' int2str(RoundNumber) '.mat']);
 Train_R=Train_R.x;
-Train_Theta=load(['Training_Theta' '__' int2str(RoundNumber) '.mat']);
+Train_Theta=load(['Training_Aug27_Theta' '__' int2str(RoundNumber) '.mat']);
 Train_Theta=Train_Theta.x;
-Train_Xe=load(['Training_Xe' '__' int2str(RoundNumber) '.mat']);
+Train_Xe=load(['Training_Aug27_Xe' '__' int2str(RoundNumber) '.mat']);
 Train_Xe=Train_Xe.x;
-Train_Xc=load(['Training_Xc' '__' int2str(RoundNumber) '.mat']);
+Train_Xc=load(['Training_Aug27_Xc' '__' int2str(RoundNumber) '.mat']);
 Train_Xc=Train_Xc.x;
 
 %Register 2-36 eye image with 1st eye image, resize and save feature
 for QueryImageNumber=1:36
     MovingPoints=[Train_Xe(2,QueryImageNumber)-cos(Train_Theta(QueryImageNumber))*Train_B2(QueryImageNumber), Train_Xe(1,QueryImageNumber)+sin(Train_Theta(QueryImageNumber))*Train_B2(QueryImageNumber); Train_Xe(2,QueryImageNumber)+cos(Train_Theta(QueryImageNumber))*Train_B2(QueryImageNumber),Train_Xe(1,QueryImageNumber)-sin(Train_Theta(QueryImageNumber))*Train_B2(QueryImageNumber)];
     MovingPoints=round(MovingPoints);
-    QueryImage=imread(['test_', ImageName,int2str(QueryImageNumber),'__', int2str(RoundNumber), ImageExtension]);
+    QueryImage=imread([ImageName,int2str(QueryImageNumber),'__', int2str(RoundNumber), ImageExtension]);
     tform = fitgeotrans(MovingPoints,FixedPoints,'nonreflectivesimilarity');
     QueryRegisteredImage=imwarp(QueryImage,tform,'OutputView',imref2d(size(TemplateImage)));
     figure(2);
@@ -84,13 +85,13 @@ for QueryImageNumber=1:36
     QueryCroppedImage=QueryRegisteredImage(YRange(1):YRange(2),FixedPoints(1,1):FixedPoints(2,1));
     figure(4);
     imshowpair(CroppedImage,QueryCroppedImage,'montage');
-    imwrite(QueryCroppedImage,['enlarged_registeredEyes_left_',int2str(QueryImageNumber),'__', int2str(RoundNumber),'.jpg']);
+    imwrite(QueryCroppedImage,['new_data/enlarged_registeredEyes_Aug27_left_',int2str(QueryImageNumber),'__', int2str(RoundNumber),'.jpg']);
     RegisteredResizedImage=imresize(QueryCroppedImage, Resize);
     ImageVector=[];
     for i=1:size(RegisteredResizedImage,1)
         ImageVector=[ImageVector RegisteredResizedImage(i,:)];
     end
     x.x=double(ImageVector')./255;
-    save(['enlarged_RegisteredFeature_left_', int2str(QueryImageNumber-1),'__', int2str(RoundNumber), '.mat'],'-struct','x');
+    save(['new_data/enlarged_RegisteredFeature_Aug27_left_', int2str(QueryImageNumber-1),'__', int2str(RoundNumber), '.mat'],'-struct','x');
     pause;
 end
